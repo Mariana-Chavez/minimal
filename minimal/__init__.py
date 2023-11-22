7bc9b6a2b194de3a69bbdfb1dc74f86e26842a4c
Mariana Chavez <141682590+Mariana-Chavez@users.noreply.github.com>
2023-11-22 14:44:41 -0600

    uno __init__.py

--- minimal/__init__.py
+++ minimal/__init__.py
@@ -6,3 +6,12 @@ def my_sum(x, y):
 def my_mul(x, y):
     """A function that multiply. """
     return x*y
+
+def ternas(n):
+    t= []
+    for x in range(1,n):
+      for y in range(x+1,n):
+        for z in range(y+1,n):
+          if x**2+y**2== z**2:
+            t.append((x,y,z))
+    return t
